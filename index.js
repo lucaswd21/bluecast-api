f7393b463873597e39f393f01f2464507dffd44b
lucaswd21 <lucas.wd.wd@gmail.com>
2021-07-01 11:05:05 -0300

    make it better

--- index.js
+++ index.js
@@ -5,28 +5,32 @@ const mongoose = require('mongoose');
 
 const app = express()
 
-mongoose.connect('mongodb://localhost/tryiotest', { useNewUrlParser:true, useUnifiedTopology: true }).then(() => {
-  mongoose.Promise = global.Promise
+try {
+  mongoose.connect('mongodb://localhost/tryiotest', { useNewUrlParser:true, useUnifiedTopology: true }).then(() => {
+    mongoose.Promise = global.Promise
 
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Access-Control-Allow-Origin, Accept, Authorization");
-    next();
+    app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Access-Control-Allow-Origin, Accept, Authorization");
+      next();
+    });
+
+    app.use(express.json());
+
+    app.use('/api', routes);
+
+    app.use((err, req, res, next) => {
+      res.status(422).send({error: err.message})
+    })
+
+    var server_port = process.env.PORT || 80;
+    var server_host = process.env.HOST || '0.0.0.0';
+
+    app.listen(server_port, server_host, () => {
+      console.log('Server online!')
+    })
   });
-
-  app.use(express.json());
-
-  app.use('/api', routes);
-
-  app.use((err, req, res, next) => {
-    res.status(422).send({error: err.message})
-  })
-
-  var server_port = process.env.PORT || 80;
-  var server_host = process.env.HOST || '0.0.0.0';
-
-  app.listen(server_port, server_host, () => {
-    console.log('Server online!')
-  })
-});
+} catch (error) {
+  throw error
+}
